3dddceae114e67a8c37d31c3a20674c1db3e6013
Jenkins <james@somehero.com>
2013-03-04 20:58:41 -0500

    added association between teams and leagues

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ActiveRecord::Base
   attr_accessible :city_name, :league_id, :name, :primary_color, :second_color, :state_abbreviation, :team_name, :team_short_name
+  belongs_to:league
 end
